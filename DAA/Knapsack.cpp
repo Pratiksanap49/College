e4e0ee9eef9111a5cde22787a9b9363ba6fd76d7
Pratiksanap49 <pratiksanap789@gmail.com>
2025-10-04 13:20:09 +0530

    First commit

--- DAA/Knapsack.cpp
+++ DAA/Knapsack.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void calculateratio(int weight[] , int profit[], float ratio[] ,int numberOfItems){
+
+    for(int i=0 ; i<numberOfItems ; i++){
+        ratio[i] = (float) profit[i] / weight[i];
+        cout<<ratio[i];
+        cout<<endl;
+    }
+}
+
 int main(){
-    int weight , numberOfItems , profit ;
+    int numberOfItems  ;
+    cout<<"Number of items : ";
+    cin>>numberOfItems;
+    int weight[numberOfItems] , profit[numberOfItems] ; float ratio[numberOfItems];
+
+    for(int i=0 ; i<numberOfItems ; i++){
+        cout<<"Weight of item "<<i+1<<" : ";
+        cin>>weight[i];
+        cout<<"Profit of item "<<i+1<<" : ";
+        cin>>profit[i];
+    }
+
+    calculateratio(weight,profit,ratio,numberOfItems);
+
+    return 0;
     
 }
